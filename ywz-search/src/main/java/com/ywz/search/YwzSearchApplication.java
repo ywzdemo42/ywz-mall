a686aa58e52c8212673e28d9e5969e2d56ddaa13
ywzdemo42 <408083094@qq.com>
2020-02-21 00:43:31 +0800

    商品搜索服务

---
+++ ywz-search/src/main/java/com/ywz/search/YwzSearchApplication.java
@@ -0,0 +1,15 @@
+package com.ywz.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class YwzSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YwzSearchApplication.class);
+    }
+}
